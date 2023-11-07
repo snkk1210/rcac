6a321702abdfb817c48d9f07180e76a7dc07c266
snkk1210 <n41210guitar@gmail.com>
2023-11-07 21:21:30 +0900

    modify: argparse

--- bin/main.py
+++ bin/main.py
@@ -1,9 +1,24 @@
 import os
 import sys
+import argparse
 
 module_path = os.path.abspath("../src/rcac/app.py")
 sys.path.append(os.path.dirname(module_path))
 
 import app
 
-app.lambda_handler(" ", " ")
+parser = argparse.ArgumentParser(
+    usage='Specify the type in -t as arguments. Check the help ( -h ) section for details.',
+    description='Check the cost of your AWS account.'
+    )
+parser.add_argument('-t', '--type', required=True, help='Specify the type (1: Notify to Slack or 2: Notify to stdout)')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    if args.type == "1":
+        app.lambda_handler(" ", " ")
+    elif args.type == "2":
+        sys.exit(1)
+    else:
+        print('Invalid argument.')
+        sys.exit(1)
